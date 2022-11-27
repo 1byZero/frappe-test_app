43e556280627b6f856f97a79323fd1569318915b
1byZero <amitkp66@live.com>
2022-11-27 18:15:09 +0000

    basic

---
+++ test_app/api.py
@@ -0,0 +1,20 @@
+import frappe
+
+@frappe.whitelist()
+def execute_function(*args,**kwargs):
+    """
+    This fonction will be executed when the Execute Action Button will be clicked
+    """
+    print('Hello World')
+    # The data is transmitted via keyword argument
+    print(kwargs)
+
+
+@frappe.whitelist()
+def execute_buy_function(*args,**kwargs):
+    """
+    This fonction will be executed when the Execute Action Button will be clicked
+    """
+    print('Hello World')
+    # The data is transmitted via keyword argument
+    print(kwargs)
